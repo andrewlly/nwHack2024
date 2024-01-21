138bc78d95d73b23916a3d56ee7172775c7f0069
andrewlly <andrewl02y@gmail.com>
2024-01-21 06:02:15 -0800

    dashboard api attempt 3

--- src/main/java/com/github/nwhack2024/web/DashboardController.java
+++ src/main/java/com/github/nwhack2024/web/DashboardController.java
@@ -3,14 +3,20 @@ package com.github.nwhack2024.web;
 import com.github.nwhack2024.domain.DisplayPlant;
 import com.github.nwhack2024.domain.PlantTask;
 import com.github.nwhack2024.domain.ResponseResult;
+import com.github.nwhack2024.domain.entity.UserTasks;
 import com.github.nwhack2024.service.UserPlantService;
+import com.github.nwhack2024.service.UserTasksService;
 import jakarta.annotation.Resource;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,6 +27,9 @@ public class DashboardController {
 
     @Resource
     UserPlantService userPlantService;
+
+    @Resource
+    UserTasksService userTasksS;
 
 
     @GetMapping("/dashboard")
@@ -34,11 +43,30 @@ public class DashboardController {
             plantsByUserName = userPlantService.getPlantsByUserName(userDetails.getUsername());
             plantTasks = userPlantService.getPlantTask(userDetails.getUsername());
         } catch (Exception e){
-            return new ResponseResult<>(403, "Failed login");
+            return new ResponseResult<>(402, "Info Not exist");
         }
         Map<String, Object> data = new HashMap<>();
         data.put("task",plantTasks);
         data.put("plants",plantsByUserName);
-        return new ResponseResult<>(200, "loginSuccess", data);
+        return new ResponseResult<>(200, "data fetched for login", data);
+    }
+
+    @PostMapping("dashboard")
+    public ResponseResult checkTask(@RequestBody UserTasks userTasks) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String username = userDetails.getUsername();
+        userTasks.setCompleted(1);
+        try{
+            userTasksS.updateById(userTasks);
+        } catch (Exception e){
+            return new ResponseResult<>(402, "Info Not exist");
+        }
+        Date currentDate = new Date();
+        userTasks.setCompleted(0);
+        userTasks.setCreationDate(currentDate);
+        userTasks.setUserTasksId(null);
+        userTasksS.save(userTasks);
+        return new ResponseResult<>(200, "checkSuccess");
     }
 }
