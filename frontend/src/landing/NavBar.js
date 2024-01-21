7d7219451c833b4da067932bb9ab471526223381
LenaWang03 <xianna03@gmail.com>
2024-01-21 03:14:34 -0800

    styled landing and login

--- frontend/src/landing/NavBar.js
+++ frontend/src/landing/NavBar.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Landing.css';
+import logo from "../assets/logo.svg";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="logo-container">
-        <div className="logo-icon"></div>
+        <img alt="logo" src={logo} />
         <span className="logo-text">BestBuds</span>
       </div>
       <div className="navbar-items-container">
-        <p className="navbar-item">About</p>
-        <p className="navbar-item">How It Works</p>
+        {/* <p className="navbar-item">About</p>
+        <p className="navbar-item">How It Works</p> */}
         <Link to="/login">
-          <button className="navbar-item login-button">Log In</button>
+          <button className="navbar-item login-button">LOG IN</button>
         </Link>
       </div>
     </nav>
