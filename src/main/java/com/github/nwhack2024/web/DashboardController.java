9e552e333296286542b21249ee3fd717592be1cf
andrewlly <andrewl02y@gmail.com>
2024-01-20 19:31:02 -0800

    DashboardController skeleton

---
+++ src/main/java/com/github/nwhack2024/web/DashboardController.java
@@ -0,0 +1,16 @@
+package com.github.nwhack2024.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController("/dashboard")
+public class DashboardController {
+//    @GetMapping("/{userId}")
+//    public ResponseEntity<Map<String, Object>> getNewsById(@PathVariable Long userId) {
+//        return BaseController.getById(newsId, newsService::getById);
+//    }
+}
