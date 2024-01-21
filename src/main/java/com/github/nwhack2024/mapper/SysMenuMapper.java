ee662f2c3bb92906fe35509d609bbfa3dd1c5f29
andrewlly <andrewl02y@gmail.com>
2024-01-20 17:20:44 -0800

    RBAC model setup ready

--- src/main/java/com/github/nwhack2024/mapper/SysMenuMapper.java
+++ src/main/java/com/github/nwhack2024/mapper/SysMenuMapper.java
@@ -3,7 +3,10 @@ package com.github.nwhack2024.mapper;
 import com.github.nwhack2024.domain.SysMenu;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
+import java.util.Set;
+
 public interface SysMenuMapper extends BaseMapper<SysMenu> {
+    Set<SysMenu> selectAllByRoleId(Set<Long> roleSet);
 
 }
 
--- src/main/java/com/github/nwhack2024/mapper/SysRoleMapper.java
+++ src/main/java/com/github/nwhack2024/mapper/SysRoleMapper.java
@@ -3,7 +3,11 @@ package com.github.nwhack2024.mapper;
 import com.github.nwhack2024.domain.SysRole;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
+import java.util.Set;
+
 public interface SysRoleMapper extends BaseMapper<SysRole> {
+    Set<SysRole> selectRoleByUserID(long id);
+    Set<String> selectRoleLabelByUserID(long id);
 
 }
 
