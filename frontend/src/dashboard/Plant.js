b45a1c2ad6e6c7e4c51f198250a29f5d98c15ae2
isa-leroux448 <isa.leroux448@gmail.com>
2024-01-20 23:26:42 -0800

    add dropdown

--- frontend/src/dashboard/Plant.js
+++ frontend/src/dashboard/Plant.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Dashboard.css';
 import ProgressBar from './ProgressBar';
 
 const Plant = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  }
+  
   return (
-    <div className="plant-container">
+    
+    <div className="plant-container" onClick={handleClick}>
       <div className="plant-count-badge">2</div>
       <div className="plant-circle"></div>
       <div className="plant-info-container">
         <h3 className="plant-name">Jerry - <span className="plant-type">Tomato Plant</span></h3>
         <ProgressBar />
+        {isOpen && (<div>
+          <div className='health-status'>Healthy</div>
+          <p>Cake<span> January 20th, 2024</span></p>
+          <p><span>Leaf </span>Next stage is in<span>3 weeks!</span></p>
+          <p><span>Drop </span>Next watering is in<span>3 days!</span></p>
+          <div className='upgrade-button'>Upgrade This Plant to Next Stage</div>
+          <p className="learn-more-button" style={{ textDecoration: 'underline' }}><span>i</span>Learn more about this plant!</p>
+        </div>)}
       </div>
     </div>
   );
