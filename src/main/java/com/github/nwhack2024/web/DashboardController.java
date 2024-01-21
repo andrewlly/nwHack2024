17a8cde9d886f92524f0009a3cf193d21038ccdb
andrewlly <andrewl02y@gmail.com>
2024-01-21 03:10:26 -0800

    fix login

--- src/main/java/com/github/nwhack2024/web/DashboardController.java
+++ src/main/java/com/github/nwhack2024/web/DashboardController.java
@@ -1,10 +1,9 @@
 package com.github.nwhack2024.web;
 
 import com.github.nwhack2024.domain.DisplayPlant;
-import com.github.nwhack2024.mapper.UserMapper;
+import com.github.nwhack2024.domain.ResponseResult;
 import com.github.nwhack2024.service.UserPlantService;
 import jakarta.annotation.Resource;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Map;
 
 
 @RestController
@@ -23,12 +21,16 @@ public class DashboardController {
 
 
     @GetMapping("/dashboard")
-    public ResponseEntity<Map<String, Object>> getDashBoardInfo() {
+    public ResponseResult getDashBoardInfo() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         String username = userDetails.getUsername();
-        List<DisplayPlant> plantsByUserName = userPlantService.getPlantsByUserName(userDetails.getUsername());
-        System.out.println(username);
-        return null;
+        List<DisplayPlant> plantsByUserName;
+        try{
+            plantsByUserName = userPlantService.getPlantsByUserName(userDetails.getUsername());
+        } catch (Exception e){
+            return new ResponseResult<>(403, "Failed login");
+        }
+        return new ResponseResult<>(200, "loginSuccess", plantsByUserName);
     }
 }
