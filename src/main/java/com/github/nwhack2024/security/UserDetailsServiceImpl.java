e4ac214f07575d17063da8665a0263c9f5a388c6
andrewlly <andrewl02y@gmail.com>
2024-01-20 17:59:24 -0800

    Auth setup ready

--- src/main/java/com/github/nwhack2024/security/UserDetailsServiceImpl.java
+++ src/main/java/com/github/nwhack2024/security/UserDetailsServiceImpl.java
@@ -1,52 +1,52 @@
-//package com.github.nwhack2024.security;
-//
-//import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-//import com.github.nwhack2024.domain.SysMenu;
-//import com.github.nwhack2024.domain.SysRole;
-//import com.github.nwhack2024.domain.User;
-//import com.github.nwhack2024.mapper.SysMenuMapper;
-//import com.github.nwhack2024.mapper.SysRoleMapper;
-//import com.github.nwhack2024.mapper.UserMapper;
-//import jakarta.annotation.Resource;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.*;
-//
-//@Service
-//public class UserDetailsServiceImpl implements UserDetailsService {
-//    @Resource
-//    private UserMapper userMapper;
-//    @Resource
-//    private SysMenuMapper menuMapper;
-//
-//    @Resource
-//    private SysRoleMapper roleMapper;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String email) {
-//        LambdaQueryWrapper<User> lambdaQueryWrapper = new LambdaQueryWrapper<>();
-//        lambdaQueryWrapper.eq(User::getEmail, email);
-//        User user = userMapper.selectOne(lambdaQueryWrapper);
-//        if(Objects.isNull(user)){
-//            throw new RuntimeException("invalid account");
-//        }
-//        Set<SysRole> roles = roleMapper.selectRoleByUserID(user.getUid());
-//        Set<String> roleLabels = new HashSet<>();
-//        Set<Long> roleIds = new HashSet<>();
-//        for(SysRole role: roles){
-//            roleLabels.add(role.getRole_label());
-//            roleIds.add(role.getId());
-//        }
-//        Set<SysMenu> menus = menuMapper.selectAllByRoleId(roleIds);
-//        List<String> perms= new ArrayList<>();
-//        for (SysMenu m: menus) {
-//            perms.add(m.getPerms());
-//        }
-//
-//        return new LoginUser(user,perms,roleLabels);
-//    }
-//
-//
-//}
+package com.github.nwhack2024.security;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.github.nwhack2024.domain.SysMenu;
+import com.github.nwhack2024.domain.SysRole;
+import com.github.nwhack2024.domain.User;
+import com.github.nwhack2024.mapper.SysMenuMapper;
+import com.github.nwhack2024.mapper.SysRoleMapper;
+import com.github.nwhack2024.mapper.UserMapper;
+import jakarta.annotation.Resource;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Resource
+    private UserMapper userMapper;
+    @Resource
+    private SysMenuMapper menuMapper;
+
+    @Resource
+    private SysRoleMapper roleMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        LambdaQueryWrapper<User> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(User::getEmail, email);
+        User user = userMapper.selectOne(lambdaQueryWrapper);
+        if(Objects.isNull(user)){
+            throw new RuntimeException("invalid account");
+        }
+        Set<SysRole> roles = roleMapper.selectRoleByUserID(user.getUid());
+        Set<String> roleLabels = new HashSet<>();
+        Set<Long> roleIds = new HashSet<>();
+        for(SysRole role: roles){
+            roleLabels.add(role.getRoleLabel());
+            roleIds.add(role.getId());
+        }
+        Set<SysMenu> menus = menuMapper.selectAllByRoleId(roleIds);
+        List<String> perms= new ArrayList<>();
+        for (SysMenu m: menus) {
+            perms.add(m.getPerms());
+        }
+
+        return new LoginUser(user,perms,roleLabels);
+    }
+
+
+}
